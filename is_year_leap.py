d2626eab6135a9edff5713481fca7a8f547aa4d8
Nahom <112133958+Nahomatnafu@users.noreply.github.com>
2022-12-11 18:55:05 -0600

    Add files via upload

---
+++ is_year_leap.py
@@ -0,0 +1,20 @@
+"""
+Written by Nahom Atnafu
+
+"""
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("It's a leap year.")
+            else:
+                print("Not a leap year.")
+        else:
+            print("It's a leap year.")
+    else:
+        print("Not a leap year.")
+
+
+year = int(input('Enter a year: '))
+is_leap(year)
